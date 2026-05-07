5f681a749a6691c924f68c4e96f63d4b3db291c0
Shamala Charitha Reddy <charitha@Mac.lan>
2026-05-07 15:22:32 +0530

    Added User model

--- backend/Models/User.js
+++ backend/Models/User.js
@@ -1,24 +1,21 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema(
-{
-    name: {
-        type: String,
-        required: true,
-    },
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
 
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
 
-    password: {
-        type: String,
-        required: true,
-    },
-},
-{ timestamps: true }
-);
+  password: {
+    type: String,
+    required: true,
+  },
+});
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", UserSchema);
